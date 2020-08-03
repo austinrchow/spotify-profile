4531b5e99ada1ec920d6c8b81924e6475ac45bba
Austin Chow <archow@stanford.edu>
2020-08-03 13:22:59 -0700

    changed login url

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 
+const login_url =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:8888/login"
+    : "https://spotify-prof-backend.herokuapp.com/login";
 const Login = () => {
   return (
     <div>
       {/* <a href="http://localhost:8888/login"> */}
-      <button onClick={() => (window.location = "http://localhost:8888/login")}>
+      <button onClick={() => (window.location = { login_url })}>
         Login With Spotify
       </button>
       {/* </a> */}
